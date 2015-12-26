b4f93e43c137acca2cc6a94c8f8babdb3d07965f
Markus Barthlen <you@example.com>
2015-12-26 11:19:38 +0100

    stand

---
+++ src/de/barthlen/experiments/Main.java
@@ -0,0 +1,19 @@
+package de.barthlen.experiments;
+
+import java.io.Serializable;
+
+public class Main implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 729168060447076758L;
+	public static String linelengthexceeded;
+
+	public static String test() {
+		if ("" == "") {
+			return "PMD does not work";
+		}
+		return "";
+	}
+}
